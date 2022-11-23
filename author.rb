81cf7a93687f9b652e01e098779d972eb76b7161
asnganje <abd.nganje@gmail.com>
2022-11-23 22:25:50 +0300

    fix linter errors

--- author.rb
+++ author.rb
@@ -1,16 +1,16 @@
 class Author < Item
-    attr_reader :id, :first_name, :last_name
-    attr_accessor :items
+  attr_reader :id, :first_name, :last_name
+  attr_accessor :items
 
-    def initialize(first_name, last_name, id = rand(1..1000))
-        @first_name = first_name
-        @last_name = last_name
-        @id = id
-        @items = []
-    end
+  def initialize(first_name, last_name, id = rand(1..1000))
+    @first_name = first_name
+    @last_name = last_name
+    @id = id
+    @items = []
+  end
 
-    def add_item(item)
-        @items << item
-        item.add_author(self)
-    end
+  def add_item(item)
+    @items << item
+    item.add_author(self)
+  end
 end
--- game.rb
+++ game.rb
@@ -1,13 +1,12 @@
 class Game
-    attr_reader :multiplayer, :last_played_at
+  attr_reader :multiplayer, :last_played_at
 
-    def initialize(multiplayer = false, last_played_at = Time.new)
-        @multiplayer = multiplayer
-        @last_played_at = last_played_at
-    end
+  def initialize(multiplayer = false, last_played_at = Time.new)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
 
-    def can_be_archived?
-        super && @last_played_at > 2.years.ago
-    end
+  def can_be_archived?
+    super && @last_played_at > 2.years.ago
+  end
 end
-
