44fb177b45dd685d6d3892f8edbced82537a2bd3
lucabenaventew <cacachuuuluca@gmail.com>
2022-11-25 10:39:14 -0500

    Fix Linters

--- user_interface/goodbye.rb
+++ user_interface/goodbye.rb
@@ -1,7 +1,7 @@
 class Goodbye
-    def self.goodbye
-      puts "\nSee you later! :)"
-      puts 'Authors: Tamana | Abdulrahman | Luca'.colorize(color: :blue).bold
-      puts ''
-    end
+  def self.goodbye
+    puts "\nSee you later! :)"
+    puts 'Authors: Tamana | Abdulrahman | Luca'.colorize(color: :blue).bold
+    puts ''
   end
+end
--- user_interface/greetings.rb
+++ user_interface/greetings.rb
@@ -1,5 +1,5 @@
 class Greetings
-    def self.greetings
-      puts "\nWelcome to my catalog!".colorize(color: :blue).bold
-    end
+  def self.greetings
+    puts "\nWelcome to my catalog!".colorize(color: :blue).bold
   end
+end
